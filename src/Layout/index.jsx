6b71a1d114953ea32b33171dfb491a8cf4aee064
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-10-07 20:40:04 +0400

    feat:add header and footer

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer/>
+        </>
+    )
 }
 
 export default Layout
